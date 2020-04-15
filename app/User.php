804c5e23571615a74db48385e15b4803f2fe4189
Hasinur Rahman <likhon@gmail.com>
2020-04-16 00:57:29 +0600

    Add author of the question and created date

--- app/User.php
+++ app/User.php
@@ -39,5 +39,9 @@ class User extends Authenticatable
     public function questions(){
         return $this->hasMany(Question::class);
     }
+    public function getUrlAttribute(){
+        //return route('questions.view', $this->id);
+        return "#";
+    }
 
 }
