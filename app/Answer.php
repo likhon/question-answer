46c242fa487dc2e386220a30629d75a9cc6152ae
Hasinur Rahman <likhon@gmail.com>
2020-04-26 01:07:52 +0600

    Update best_answer_id in question table when delete and answer in second method(onDelete set null in database)

--- app/Answer.php
+++ app/Answer.php
@@ -22,12 +22,12 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
         static::deleted(function ($answer){
-           $question = $answer->question;
-           $question->decrement('answers_count');
-           if ($question->best_answer_id == $answer->id){
-               $question->best_answer_id = NULL;
-               $question->save();
-           }
+//           $question = $answer->question;
+           $answer->question->decrement('answers_count');
+//           if ($question->best_answer_id == $answer->id){
+//               $question->best_answer_id = NULL;
+//               $question->save();
+//           }
         });
     }
 
