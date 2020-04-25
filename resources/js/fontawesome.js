0f2dfdb3af44453dbf16eb45e340f98609cd486c
Hasinur Rahman <likhon@gmail.com>
2020-04-25 16:23:21 +0600

    Add voting UI

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+//import { faUser } from '@fortawesome/free-solid-svg-icons'
+import {faCaretUp} from '@fortawesome/free-solid-svg-icons/faCaretUp'
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons/faCaretDown'
+import {faStar} from '@fortawesome/free-solid-svg-icons/faStar'
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck'
+
+library.add(faCaretUp,faCaretDown,faStar,faCheck);
+
+
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,25 +5,39 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h1>{{$question->title}}</h1>
-                            <div class="ml-auto">
-                                <a href="{{route('questions.index')}}" class="btn btn-outline-primary">Back to all Question</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h1>{{$question->title}}</h1>
+                                <div class="ml-auto">
+                                    <a href="{{route('questions.index')}}" class="btn btn-outline-primary">Back to all Question</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div class="card-body">
-                        {!!$question->body_html!!}
-                        <div class="float-right">
-                            <span class="text-muted">{{ $question->created_date }}</span>
-                            <div class="media">
-                                <a href="{{ $question->user->url }}" class="pr-2">
-                                    <img src="{{ $question->user->avatar }}">
+                        <hr>
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a class="vote-up" title="This question is useful"><i class="fas fa-caret-up fa-3x"></i></a>
+                                <span class="votes-count">1230</span>
+                                <a class="vote-down off" title="This question is not useful"><i class="fas fa-caret-down fa-3x"></i></a>
+                                <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                                    <i class="fas fa-star fa-2x"></i>
+                                <span class="favorites-count">123</span>
                                 </a>
-                                <div class="media-body">
-                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+
+                            </div>
+                            <div class="media-body">
+                                {!!$question->body_html!!}
+                                <div class="float-right">
+                                    <span class="text-muted">{{ $question->created_date }}</span>
+                                    <div class="media">
+                                        <a href="{{ $question->user->url }}" class="pr-2">
+                                            <img src="{{ $question->user->avatar }}">
+                                        </a>
+                                        <div class="media-body">
+                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -40,23 +54,32 @@
                         </div>
                         <hr>
                         @foreach($question->answers as $answer)
-                            <div class="media">
-                                <div class="media-body">
-                                    {!! $answer->body_html !!}
-                                    <div class="float-right">
-                                        <span class="text-muted">{{ $answer->created_date }}</span>
-                                        <div class="media">
-                                            <a href="{{ $answer->user->url }}" class="pr-2">
-                                                <img src="{{ $answer->user->avatar }}">
-                                            </a>
-                                            <div class="media-body">
-                                                <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                            </div>
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a class="vote-up" title="This answer is useful"><i class="fas fa-caret-up fa-3x"></i></a>
+                                <span class="votes-count">1230</span>
+                                <a class="vote-down off" title="This answer is not useful"><i class="fas fa-caret-down fa-3x"></i></a>
+                                <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+
+                            </div>
+                            <div class="media-body">
+                                {!! $answer->body_html !!}
+                                <div class="float-right">
+                                    <span class="text-muted">{{ $answer->created_date }}</span>
+                                    <div class="media">
+                                        <a href="{{ $answer->user->url }}" class="pr-2">
+                                            <img src="{{ $answer->user->avatar }}">
+                                        </a>
+                                        <div class="media-body">
+                                            <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <hr>
+                        </div>
+                        <hr>
                         @endforeach
                     </div>
                 </div>
