c21241cfc6a5a9daa59c77b90d0b0cc6ea0013a4
Hasinur Rahman <likhon@gmail.com>
2020-05-11 23:45:57 +0600

    Implement many to many polymorphic relationship and make the vote qiestion controls working

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -17,9 +17,27 @@
                         <hr>
                         <div class="media">
                             <div class="d-flex flex-column vote-controls">
-                                <a class="vote-up" title="This question is useful"><i class="fas fa-caret-up fa-3x"></i></a>
-                                <span class="votes-count">1230</span>
-                                <a class="vote-down off" title="This question is not useful"><i class="fas fa-caret-down fa-3x"></i></a>
+                                <a title="This question is useful"
+                                   class="vote-up {{ auth()->guest() ? 'off' : ''}}"
+                                   onclick="event.preventDefault(); document.getElementById('up-vote-question-{{$question->id}}').submit();">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <form id="up-vote-question-{{$question->id}}" action="/questions/{{$question->id}}/vote" method="POST" style="display: none">
+                                    @csrf
+                                    <input type="hidden" name="vote" value="1">
+
+                                </form>
+                                <span class="votes-count">{{$question->votes_count}}</span>
+                                <a title="This question is not useful"
+                                   class="vote-down {{ auth()->guest() ? 'off' : ''}}"
+                                   onclick="event.preventDefault(); document.getElementById('down-vote-question-{{$question->id}}').submit();">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <form id="down-vote-question-{{$question->id}}" action="/questions/{{$question->id}}/vote" method="POST" style="display: none">
+                                    @csrf
+                                    <input type="hidden" name="vote" value="-1">
+
+                                </form>
                                 <a title="Click to mark as favorite question (Click again to undo)"
                                    class="favorite mt-2 {{auth()->guest() ? 'off' : ($question->is_favorited ? 'favorited':'')}}"
                                    onclick="event.preventDefault(); document.getElementById('favorite-question-{{$question->id}}').submit();">
